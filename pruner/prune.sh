0ecf509f3d2364acac89d0800e5edc79bdd39da8
Patrick Jeremic <pjeremic@chorus.one>
2025-08-29 09:50:22 +0200

    Revert folder change and improve ergonomics

--- pruner/prune.sh
+++ pruner/prune.sh
@@ -3,16 +3,17 @@
 DELETE_OLDER_THAN_DAYS=$1
 PERIOD_SECONDS="${2:-86400}" # Default: 24 hrs
 
-DATA_PATH="/home/hluser/hl"
+# Only prune data directory, not the new `hyperliquid_data` directory
+DATA_PATH="/home/hluser/hl/data"
 
 echo "Will prune every $PERIOD_SECONDS seconds"
 echo "Will delete files in data directory older than $DELETE_OLDER_THAN_DAYS days"
 
 while true; do
+    # prune before sleeping
+    echo "Deleting files in data directory older than $DELETE_OLDER_THAN_DAYS days"
+    find "$DATA_PATH" -mindepth 1 -depth -mtime +$DELETE_OLDER_THAN_DAYS -delete
+
     echo "Sleeping for $PERIOD_SECONDS seconds"
     sleep $PERIOD_SECONDS
-
-    echo "Deleting files in data directory older than $DELETE_OLDER_THAN_DAYS days"
-    find "$DATA_PATH/data" -mindepth 1 -depth -mtime +$DELETE_OLDER_THAN_DAYS -delete
-    find "$DATA_PATH/hyperliquid_data" -mindepth 1 -depth -mtime +$DELETE_OLDER_THAN_DAYS -delete
 done
